6579c98aed9afd9c65d0eca36d7e610a30ffbd46
Matthias Krauss <matthias@presseverykey.com>
2024-10-24 04:39:17 +0200

    Added create, update, delete logic

--- database.js
+++ database.js
@@ -4,38 +4,50 @@ let db = null;
 
 const createRideTable = `
 CREATE TABLE IF NOT EXISTS ride (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	uuid TEXT,
-	startDate REAL,
-	endDate REAL,
-	dist REAL,
-	motionDist REAL,
-	duration REAL,
-	motionDuration REAL,
-	maxSpeed REAL,
-	publicKey TEXT,
-	rideType INTEGER,
-	vehicleType INTEGER,
-	mountType INTEGER,
-	flags INTEGER,
-	comment TEXT
+	id INTEGER PRIMARY KEY,
+	uuid TEXT NOT NULL UNIQUE,
+	startDate REAL NOT NULL,
+	endDate REAL NOT NULL,
+	dist REAL NOT NULL,
+	motionDist REAL NOT NULL,
+	duration REAL NOT NULL,
+	motionDuration REAL NOT NULL,
+	maxSpeed REAL NOT NULL,
+	publicKey TEXT NOT NULL,
+	rideType INTEGER NOT NULL,
+	vehicleType INTEGER NOT NULL,
+	mountType INTEGER NOT NULL,
+	flags INTEGER NOT NULL,
+	comment TEXT NOT NULL
 )`;
 
 const createLocationTable = `
 CREATE TABLE IF NOT EXISTS location(
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	rideId INTEGER,
-	timestamp REAL,
-	latitude REAL,
-	longitude REAL,
-	accuracy REAL,
-	altitude REAL,
-	altitudeAccuracy REAL,
-	heading REAL,
-	headingAccuracy REAL,
-	speed REAL,
-	speedAccuracy REAL
+	id INTEGER PRIMARY KEY,
+	rideId INTEGER NOT NULL,
+	timestamp REAL NOT NULL,
+	latitude REAL NOT NULL,
+	longitude REAL NOT NULL,
+	accuracy REAL NOT NULL,
+	altitude REAL NOT NULL,
+	altitudeAccuracy REAL NOT NULL,
+	heading REAL NOT NULL,
+	headingAccuracy REAL NOT NULL,
+	speed REAL NOT NULL,
+	speedAccuracy REAL NOT NULL
 )`;
+
+function isNumber(val) {
+  return (typeof(val) === 'number' && isFinite(val));
+}
+
+function isString(val) {
+  return (typeof(val) === 'string');
+}
+
+function isStringOrUndefined(val) {
+  return (typeof(val) === 'string') || (typeof(val) === 'undefined');
+}
 
 async function query(sql, params) {
   return new Promise((resolve, reject) => {
@@ -56,7 +68,21 @@ async function init(filename) {
 		  const result = db.serialize(() => {
 			  db.run(createRideTable);
 		  	db.run(createLocationTable);
-        db.run(`INSERT INTO ride (uuid, publicKey) values ("myUuid", "myPubKey")`);
+        // createRide("myUuid",
+        //   {
+        //     publicKey: "myPubKey",
+        //     startDate: 123,
+        //     endDate: 456,
+        //     dist: 123.4,
+        //     motionDist: 112.3,
+        //     duration: 332,
+        //     motionDuration:321,
+        //     maxSpeed: 33.3,
+        //     rideType: 1,
+        //     vehicleType: 2,
+        //     mountType: 3,
+        //     flags: 0
+        //   });
 		  });
       console.log(`using database at ${filename}`);
     }
@@ -65,17 +91,132 @@ async function init(filename) {
 }
 
 async function getRidePublicKey(uuid) {
-  if (db === null) throw new Error(`database not initialized`);
-  const result = await query("SELECT publicKey FROM ride WHERE uuid = (?) LIMIT 1", uuid);
-  if (result.length > 0) {
-    return result[0].publicKey;
-  } else {
-    return null;
+  try {
+    if (!isString(uuid)) {
+      return null;
+    }
+    const result = await query("SELECT publicKey FROM ride WHERE uuid = (?) LIMIT 1", uuid);
+    if (result.length > 0) {
+      return result[0].publicKey;
+    } else {
+      return null;
+    }
+  } catch (e) {
+    console.log(`DB fail in getRidePublicKey: ${e}`);
+    return false;        
   }
 }
 
+async function createRide(uuid, data) {
+  try {
+    if (!isString(uuid)) {
+      return false;
+    }
+    if (!checkCreateRideData(data)) { //this might be duplicate, but we need to ensure sane input.
+      return false;
+    }
+    const comment = isString(data.comment) ? data.comment : "";
+    const sql = `INSERT INTO ride 
+    (uuid,startDate,endDate,dist,motionDist,duration,motionDuration,maxSpeed,publicKey,rideType,vehicleType,mountType,flags,comment)
+    VALUES
+    (?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+    const args = [uuid, data.startDate, data.endDate, data.dist, data.motionDist, data.duration, data.motionDuration,
+      data.maxSpeed, data.publicKey, data.rideType, data.vehicleType, data.mountType, data.flags, comment];
+    await query(sql, args);
+    //TODO: locations ***************
+    return true;
+  } catch (e) {
+    console.log(`DB fail in createRide: ${e}`);
+    return false;        
+  }
+}
+
+async function updateRide(uuid, data) {
+  try {
+    const result = await query("SELECT * FROM ride WHERE uuid = ?", uuid);
+    if (result.length < 1) {
+      return false;
+    }
+    const existing = result[0];
+
+    const sql = `UPDATE ride SET 
+    startDate = ?,
+    endDate = ?,
+    dist = ?,
+    motionDist = ?,
+    duration = ?,
+    motionDuration = ?,
+    maxSpeed = ?,
+    publicKey = ?,
+    rideType = ?,
+    vehicleType = ?,
+    mountType = ?,
+    comment = ?
+    WHERE id = ?
+    `;
+
+    const values = [
+      isNumber(data.startDate)      ? data.startDate      : existing.startDate,
+      isNumber(data.endDate)        ? data.endDate        : existing.endDate,
+      isNumber(data.dist)           ? data.dist           : existing.dist,
+      isNumber(data.motionDist)     ? data.motionDist     : existing.motionDist,
+      isNumber(data.duration)       ? data.duration       : existing.duration,
+      isNumber(data.motionDuration) ? data.motionDuration : existing.motionDuration,
+      isNumber(data.maxSpeed)       ? data.maxSpeed       : existing.maxSpeed,
+      isString(data.publicKey)      ? data.publicKey      : existing.publicKey,
+      isNumber(data.rideType)       ? data.rideType       : existing.rideType,
+      isNumber(data.vehicleType)    ? data.vehicleType    : existing.vehicleType,
+      isNumber(data.mountType)      ? data.mountType      : existing.mountType,
+      isString(data.comment)        ? data.comment        : existing.comment,
+      existing.id
+    ];
+    await query("SELECT * FROM ride WHERE uuid = ?", values); 
+    //TODO: locations **********
+    return true;
+  } catch (e) {
+    console.log(`DB fail in updateRide: ${e}`);
+    return false;    
+  }
+}
+
+async function deleteRide(uuid) {
+  try {
+    const result = await query("SELECT id FROM ride WHERE uuid = ? LIMIT 1", uuid);
+    if (result.length < 1) {
+      return true;
+    }
+    const rideId = result[0].id;
+    await query("DELETE FROM location WHERE rideId = (?)", rideId);
+    await query("DELETE FROM ride WHERE id = (?)", rideId);
+    return true;
+  } catch (e) {
+    console.log(`DB fail in deleteRide: ${e}`);
+    return false;
+  }
+}
+
+function checkCreateRideData(data) {
+  if (typeof(data) != 'object') {
+    return false;
+  }
+  return isNumber(data.startDate) &&
+    isNumber(data.endDate)        &&
+    isNumber(data.dist)           &&
+    isNumber(data.motionDist)     &&
+    isNumber(data.duration)       &&
+    isNumber(data.motionDuration) &&
+    isNumber(data.maxSpeed)       &&
+    isString(data.publicKey)      &&
+    isNumber(data.rideType)       && 
+    isNumber(data.vehicleType)    && 
+    isNumber(data.mountType)      && 
+    isStringOrUndefined(data.comment);
+}
 
 
 exports.init = init;
 exports.getRidePublicKey = getRidePublicKey;
-
+exports.createRide = createRide;
+exports.updateRide = updateRide;
+exports.deleteRide = deleteRide;
+exports.checkCreateRideData = checkCreateRideData;
--- index.js
+++ index.js
@@ -10,24 +10,69 @@ const MAX_BODY_LENGTH = '100mb';
 const PORT = 8080;
 const API_ENDPOINT = "/api/v1/rides"; 
 
-/* always POST. Contains a JSON with root entries
+/* API_ENDPOINT requests are always POST. Payload is a JSON with root entries
 - "apikey" (constant),
 - "uuid" (ride uuid),
 - "data" (encapsulated JSON as string).
 - "signature" (Base64 of SHA256 signature of data string signed with private key)
+
+Data contains a dictionary with ride information. Based on DB state and contents,
+request will be treated as create, update or delete:
+
+If data contains a field "action":"DELETE", request is treated as a DELETE request.
+The ride with this UUID, along with all its associated data, should be removed
+from the server. Attempting to DELETE a ride with an unknown UUID has no effect.
+
+Otherwise the request is treated as a CREATE or UPDATE, depending on the stored
+rides on the server. If a ride with the given UUID exists, it is considered an
+UPDATE, otherwise it is a CREATE.
+
+CREATE requests will provide a new reide-specific public key that will be stored 
+and used to authenticate following UPDATE or DELETE requests for this ride,
+ensuring that only the originator of this ride will be allowed to change its state.
+
+CREATE requests will require a full set of properties for the ride:
+  required: startDate REAL - start UNIX timestamp 
+  required: endDate REAL - end UNIX timestamp 
+  required: dist REAL - total distance in m
+  required: motionDist REAL - distance in m considered as moving in vehicle
+  required: duration REAL - total duration of recording (may slightly vary from endDate-startDate)
+  required: motionDuration REAL - duration considered as moving in vehicle
+  required: maxSpeed REAL - max speed in m/s
+  required: publicKey TEXT - public key of ride (BASE64 string)
+  required: rideType INTEGER - ride type enumeration
+  required: vehicleType INTEGER - vehicle type enumeration
+  required: mountType INTEGER - mount of recording device enumeration
+  required: flags INTEGER - ride flags bitset
+  required: locations - ARRAY of location entries
+  optional: comment TEXT - used for debugging only
+
+UPDATE requests only require the fields to be changed. However, it's a good
+idea to re-submit all properties.
+
+If a CREATE or UPDATE requests contains entries with associated data arrays
+(e.g. `locations`), all corresponding entries will be replaced. If entries of
+this type were previously stored on the server, they will be deleted. If no
+such entry exists in the request, previous data will be kept. As an example:
+When a ride with associated locations exists on the server and an UPDATE
+request without a `locations` entry is received, the old locations will be
+kept. If an UPDATE request with a new `locations` array is received (even
+an empty one), all old locations will be deleted and the new locations will
+be added instead.
+
+Malformed or invalid requests will result in a 4xx error response. Failed valid
+reqests will usually result in a 5xx error code. Successful valid requests
+will result in a 200 OK error containing a JSON {ok : true}. 
+
+Attempts to delete a non-existing ride will not change the server. Nevertheless,
+as this is regarded as a valid request, it will result in a 200 OK message.
+
 */
-
-//when data is empty object ("{}"), request is a delete
-//when data is a non-empty object, request is a create or modify
-
-let app 
 
 run();
 
 async function run() {
   await db.init('database.sqlite');
-  const pubkey = await db.getRidePublicKey('myUuid');
-  console.log(`pubkey is ${pubkey}`);
 
   const app = express();
   app.use(bodyParser.json({limit: MAX_BODY_LENGTH})); // for parsing application/json
@@ -35,9 +80,7 @@ async function run() {
   app.listen(PORT, () => {
     console.log(`My app listening on port ${PORT}`);
   })
-
 }
-
 
 async function handleRideRequest(request, response)  {
   const body = request.body;
@@ -55,18 +98,14 @@ async function handleRideRequest(request, response)  {
   if (typeof(uuid) != "string") {
     return response.status(400).send('Request bad uuid');
   }
-
   const signatureBase64 = body.signature;
   if (typeof(signatureBase64) != "string") {
     return response.status(400).send('Request bad signature');
   }
-
-
   const dataString = body.data;
   if (typeof(dataString) != "string") {
     return response.status(400).send('Request bad data');
   }
-
   let data = null;
   try {
     data = JSON.parse(dataString);
@@ -76,9 +115,11 @@ async function handleRideRequest(request, response)  {
   if (typeof(data) != "object") {
     return response.status(400).send('Request invalid data');    
   }
-
-  //TODO: if ride exists, take public key from there!
-  let publicKeyString = data.publicKey;
+  //if ride exists, take public key from there. Use provided pub key for new rides.
+  const existingPublicKeyString = await db.getRidePublicKey(uuid);
+  const rideExists = (typeof(existingPublicKeyString) == "string");
+  console.log(`ride exists : ${rideExists} pkString ${existingPublicKeyString}`);
+  const publicKeyString = rideExists ? existingPublicKeyString : data.publicKey;
   if (typeof(publicKeyString) != "string") {
     return response.status(400).send('Request invalid data pubkey');    
   }
@@ -86,24 +127,37 @@ async function handleRideRequest(request, response)  {
   try {
     publicKey = crypto.createPublicKey({key: publicKeyString, format:'pem', type: 'pkcs1', encoding:'utf8'});
   } catch (e) {
-    console.log(e.message);
+    console.log(`create public key from string failed: ${e.message}`);
     return response.status(400).send('Request malformed public key');    
   }
-
   const verify = crypto.createVerify('SHA256');
   verify.write(dataString);
   verify.end();
 
-  console.log(`publicKeyString is ${publicKeyString} len ${publicKeyString.length} hash ${crypto.createHash('md5').update(publicKeyString).digest('hex')}`);
-  console.log(`signatureBase64 is >>${signatureBase64}<< len ${signatureBase64.length} hash ${crypto.createHash('md5').update(signatureBase64).digest('hex')}`);
-  console.log(`datastring is >>${dataString}<< len ${dataString.length} hash ${crypto.createHash('md5').update(dataString).digest('hex')}`);
-
   const isVerified = verify.verify(publicKey, signatureBase64, 'base64');
-  console.log(`isVerified is ${isVerified}`);
+  if (!isVerified) {
+    return response.status(403).send('Request could not be verified');        
+  }
+  //request is verified from here.
+  const actionString = body.action;
+  
+  //handle DELETE
+  let ok = false;
 
-  //TODO: check signature
+  if (actionString === "DELETE") {
+    ok = await db.deleteRide(uuid);
+  } else if (rideExists) {
+    ok = await db.updateRide(uuid, data);
+  } else {
+    if (!db.checkCreateRideData(data)) {
+      return response.status(400).send('Incomplete dataset');        
+    }
+    ok = await db.createRide(uuid, data);
+  }
 
-
+  if (!ok) {
+    return response.status(500).send('Request could not be handled');        
+  }
 
 
   const respJson = { body : JSON.stringify(body)};
